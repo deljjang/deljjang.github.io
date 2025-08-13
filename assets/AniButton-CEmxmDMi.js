133b496246e1a0239bee6a95ae28891944ab9f35
park g s <deljjang@gmail.com>
2025-08-13 17:06:45 +0900

    home_2025-08-13

--- assets/AniButton-CEmxmDMi.js
+++
@@ -1,1 +0,0 @@
-import{j as n,m as i}from"./index-CSxN88q8.js";import{B as a}from"./Button-D9x8S5SG.js";function s({children:t,...o}){return n.jsx(a,{variant:"contained",component:i.div,whileHover:{scale:1,transition:{duration:.3}},whileTap:{scale:.9},...o,children:t})}export{s as A};
---
+++ assets/Buttons-CZ5_MryD.js
@@ -0,0 +1,53 @@
+import{j as t,d2 as r,cR as a}from"./index-BTQulWNG.js";import{C as n}from"./ComponentCard-8_7zop3Z.js";import{P as o}from"./PageBreadCrumb-Cf3-5YJd.js";import{B as e}from"./Button-DXp0lWmj.js";import{S as s}from"./box-B1SnEylq.js";import{P as i}from"./PostDetailPage-uSDpaGuZ.js";import"./toPropertyKey-DCwh5dYN.js";function z(){return t.jsxs("div",{children:[t.jsx(r,{title:`${a.appTitle} :: Buttons`,description:a.description,author:a.appOwner}),t.jsx(o,{pageTitle:"Buttons"}),t.jsxs("div",{className:"space-y-5 sm:space-y-6",children:[t.jsx(n,{title:"Primary Button",children:t.jsxs("div",{className:"flex items-center gap-5",children:[t.jsx(e,{size:"sm",variant:"primary",children:"Button Text"}),t.jsx(e,{size:"md",variant:"primary",children:"Button Text"})]})}),t.jsx(i,{postData:`<ComponentCard title="Primary Button">
+  <div className="flex items-center gap-5">
+    <Button size="sm" variant="primary">
+      Button Text
+    </Button>
+    <Button size="md" variant="primary">
+      Button Text
+    </Button>
+  </div>
+</ComponentCard>`}),t.jsx(n,{title:"Primary Button with Left Icon",children:t.jsxs("div",{className:"flex items-center gap-5",children:[t.jsx(e,{size:"sm",variant:"primary",startIcon:t.jsx(s,{className:"size-5"}),children:"Button Text"}),t.jsx(e,{size:"md",variant:"primary",startIcon:t.jsx(s,{className:"size-5"}),children:"Button Text"})]})}),t.jsx(i,{postData:`<ComponentCard title="Primary Button with Left Icon">
+<div className="flex items-center gap-5">
+  <Button
+    size="sm"
+    variant="primary"
+    startIcon={<BoxIcon className="size-5" />}
+  >
+    Button Text
+  </Button>
+  <Button
+    size="md"
+    variant="primary"
+    startIcon={<BoxIcon className="size-5" />}
+  >
+    Button Text
+  </Button>
+</div>
+</ComponentCard>`}),t.jsx(n,{title:"Primary Button with Right Icon",children:t.jsxs("div",{className:"flex items-center gap-5",children:[t.jsx(e,{size:"sm",variant:"primary",endIcon:t.jsx(s,{className:"size-5"}),children:"Button Text"}),t.jsx(e,{size:"md",variant:"primary",endIcon:t.jsx(s,{className:"size-5"}),children:"Button Text"})]})}),t.jsx(i,{postData:`<ComponentCard title="Primary Button with Right Icon">
+  <div className="flex items-center gap-5">
+    <Button
+      size="sm"
+      variant="primary"
+      endIcon={<BoxIcon className="size-5" />}
+    >
+      Button Text
+    </Button>
+    <Button
+      size="md"
+      variant="primary"
+      endIcon={<BoxIcon className="size-5" />}
+    >
+      Button Text
+    </Button>
+  </div>
+</ComponentCard>`}),t.jsx(n,{title:"Secondary Button",children:t.jsxs("div",{className:"flex items-center gap-5",children:[t.jsx(e,{size:"sm",variant:"outline",children:"Button Text"}),t.jsx(e,{size:"md",variant:"outline",children:"Button Text"})]})}),t.jsx(i,{postData:`<ComponentCard title="Secondary Button">
+  <div className="flex items-center gap-5">
+    <Button size="sm" variant="outline">
+      Button Text
+    </Button>
+    <Button size="md" variant="outline">
+      Button Text
+    </Button>
+  </div>
+</ComponentCard>`}),t.jsx(n,{title:"Outline Button with Left Icon",children:t.jsxs("div",{className:"flex items-center gap-5",children:[t.jsx(e,{size:"sm",variant:"outline",startIcon:t.jsx(s,{className:"size-5"}),children:"Button Text"}),t.jsx(e,{size:"md",variant:"outline",startIcon:t.jsx(s,{className:"size-5"}),children:"Button Text"})]})}),t.jsx(n,{title:"Outline Button with Right Icon",children:t.jsxs("div",{className:"flex items-center gap-5",children:[t.jsx(e,{size:"sm",variant:"outline",endIcon:t.jsx(s,{className:"size-5"}),children:"Button Text"}),t.jsx(e,{size:"md",variant:"outline",endIcon:t.jsx(s,{className:"size-5"}),children:"Button Text"})]})})]})]})}export{z as default};
