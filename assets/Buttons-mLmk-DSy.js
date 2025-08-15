f761de03a0f5cfce5a9ee5a63067f19cc71b267f
park g s <deljjang@gmail.com>
2025-08-15 23:57:40 +0900

    home_2025-08-15

---
+++ assets/Buttons-mLmk-DSy.js
@@ -0,0 +1,53 @@
+import{j as t,d3 as r,cR as a}from"./index-x9uRvfkR.js";import{C as i}from"./ComponentCard-Cf1zosuG.js";import{P as o}from"./PageBreadCrumb-BQAVatwG.js";import{B as e}from"./Button-5a0TlR61.js";import{S as s}from"./box-waMgYxLx.js";import{P as n}from"./PostDetailPage-DRHvOyIf.js";import{T as m}from"./TailHomeButton-CgAFhpJp.js";/* empty css                       */import"./toPropertyKey-DCwh5dYN.js";import"./TailButton-CSvNk1h6.js";function N(){return t.jsxs("div",{children:[t.jsx(r,{title:`${a.appTitle} :: Buttons`,description:a.description,author:a.appOwner}),t.jsx(m,{}),t.jsx(o,{pageTitle:"Buttons"}),t.jsxs("div",{className:"space-y-5 sm:space-y-6",children:[t.jsx(i,{title:"Primary Button",children:t.jsxs("div",{className:"flex items-center gap-5",children:[t.jsx(e,{size:"sm",variant:"primary",children:"Button Text"}),t.jsx(e,{size:"md",variant:"primary",children:"Button Text"})]})}),t.jsx(n,{postData:`<ComponentCard title="Primary Button">
+  <div className="flex items-center gap-5">
+    <Button size="sm" variant="primary">
+      Button Text
+    </Button>
+    <Button size="md" variant="primary">
+      Button Text
+    </Button>
+  </div>
+</ComponentCard>`}),t.jsx(i,{title:"Primary Button with Left Icon",children:t.jsxs("div",{className:"flex items-center gap-5",children:[t.jsx(e,{size:"sm",variant:"primary",startIcon:t.jsx(s,{className:"size-5"}),children:"Button Text"}),t.jsx(e,{size:"md",variant:"primary",startIcon:t.jsx(s,{className:"size-5"}),children:"Button Text"})]})}),t.jsx(n,{postData:`<ComponentCard title="Primary Button with Left Icon">
+<div className="flex items-center gap-5">
+  <Button
+    size="sm"
+    variant="primary"
+    startIcon={<BoxIcon className="size-5" />}
+  >
+    Button Text
+  </Button>
+  <Button
+    size="md"
+    variant="primary"
+    startIcon={<BoxIcon className="size-5" />}
+  >
+    Button Text
+  </Button>
+</div>
+</ComponentCard>`}),t.jsx(i,{title:"Primary Button with Right Icon",children:t.jsxs("div",{className:"flex items-center gap-5",children:[t.jsx(e,{size:"sm",variant:"primary",endIcon:t.jsx(s,{className:"size-5"}),children:"Button Text"}),t.jsx(e,{size:"md",variant:"primary",endIcon:t.jsx(s,{className:"size-5"}),children:"Button Text"})]})}),t.jsx(n,{postData:`<ComponentCard title="Primary Button with Right Icon">
+  <div className="flex items-center gap-5">
+    <Button
+      size="sm"
+      variant="primary"
+      endIcon={<BoxIcon className="size-5" />}
+    >
+      Button Text
+    </Button>
+    <Button
+      size="md"
+      variant="primary"
+      endIcon={<BoxIcon className="size-5" />}
+    >
+      Button Text
+    </Button>
+  </div>
+</ComponentCard>`}),t.jsx(i,{title:"Secondary Button",children:t.jsxs("div",{className:"flex items-center gap-5",children:[t.jsx(e,{size:"sm",variant:"outline",children:"Button Text"}),t.jsx(e,{size:"md",variant:"outline",children:"Button Text"})]})}),t.jsx(n,{postData:`<ComponentCard title="Secondary Button">
+  <div className="flex items-center gap-5">
+    <Button size="sm" variant="outline">
+      Button Text
+    </Button>
+    <Button size="md" variant="outline">
+      Button Text
+    </Button>
+  </div>
+</ComponentCard>`}),t.jsx(i,{title:"Outline Button with Left Icon",children:t.jsxs("div",{className:"flex items-center gap-5",children:[t.jsx(e,{size:"sm",variant:"outline",startIcon:t.jsx(s,{className:"size-5"}),children:"Button Text"}),t.jsx(e,{size:"md",variant:"outline",startIcon:t.jsx(s,{className:"size-5"}),children:"Button Text"})]})}),t.jsx(i,{title:"Outline Button with Right Icon",children:t.jsxs("div",{className:"flex items-center gap-5",children:[t.jsx(e,{size:"sm",variant:"outline",endIcon:t.jsx(s,{className:"size-5"}),children:"Button Text"}),t.jsx(e,{size:"md",variant:"outline",endIcon:t.jsx(s,{className:"size-5"}),children:"Button Text"})]})})]})]})}export{N as default};
---
+++ assets/useQuery-p3s4NQWn.js
@@ -0,0 +1,1 @@
+import{u as r,Q as u}from"./useBaseQuery-C04pDegk.js";function a(e,s){return r(e,u)}export{a as u};
