6039dc6c048611c5079a4c9ac420fd4c038dc33d
park g s <deljjang@gmail.com>
2025-09-23 14:33:29 +0900

    home_2025-09-23

---
+++ assets/Buttons-CCLzszN0.js
@@ -0,0 +1,53 @@
+import{i as c,ag as l,j as t,d0 as i}from"./index-DPOmVUnx.js";import{C as n}from"./ComponentCard-D85qWKuU.js";import{P as x}from"./PageBreadCrumb-CAuoQB64.js";import{B as e}from"./Button-AMw3IJXs.js";import{S as s}from"./box-Ccr2v4in.js";import{T as d}from"./TailHomeButton-BIcjLKws.js";import"./TwButton-DSkzl5Ql.js";function y(){const a=`<ComponentCard title="Primary Button">
+  <div className="flex items-center gap-5">
+    <Button size="sm" variant="primary">
+      Button Text
+    </Button>
+    <Button size="md" variant="primary">
+      Button Text
+    </Button>
+  </div>
+</ComponentCard>`,r=`<ComponentCard title="Primary Button with Left Icon">
+<div className="flex items-center gap-5">
+  <Button
+    size="sm"
+    variant="primary"
+    startIcon={<BoxIcon className="size-5" />}
+  >
+    Button Text
+  </Button>
+  <Button
+    size="md"
+    variant="primary"
+    startIcon={<BoxIcon className="size-5" />}
+  >
+    Button Text
+  </Button>
+</div>
+</ComponentCard>`,o=`<ComponentCard title="Primary Button with Right Icon">
+  <div className="flex items-center gap-5">
+    <Button
+      size="sm"
+      variant="primary"
+      endIcon={<BoxIcon className="size-5" />}
+    >
+      Button Text
+    </Button>
+    <Button
+      size="md"
+      variant="primary"
+      endIcon={<BoxIcon className="size-5" />}
+    >
+      Button Text
+    </Button>
+  </div>
+</ComponentCard>`,m=`<ComponentCard title="Secondary Button">
+  <div className="flex items-center gap-5">
+    <Button size="sm" variant="outline">
+      Button Text
+    </Button>
+    <Button size="md" variant="outline">
+      Button Text
+    </Button>
+  </div>
+</ComponentCard>`;return c({title:"Tailwind Buttons",description:l.description}),t.jsxs("div",{children:[t.jsx(d,{}),t.jsx(x,{pageTitle:"Buttons"}),t.jsxs("div",{className:"space-y-5 sm:space-y-6",children:[t.jsx(n,{title:"Primary Button",children:t.jsxs("div",{className:"flex items-center gap-5",children:[t.jsx(e,{size:"sm",variant:"primary",children:"Button Text"}),t.jsx(e,{size:"md",variant:"primary",children:"Button Text"})]})}),t.jsx(i,{postData:a}),t.jsx(n,{title:"Primary Button with Left Icon",children:t.jsxs("div",{className:"flex items-center gap-5",children:[t.jsx(e,{size:"sm",variant:"primary",startIcon:t.jsx(s,{className:"size-5"}),children:"Button Text"}),t.jsx(e,{size:"md",variant:"primary",startIcon:t.jsx(s,{className:"size-5"}),children:"Button Text"})]})}),t.jsx(i,{postData:r}),t.jsx(n,{title:"Primary Button with Right Icon",children:t.jsxs("div",{className:"flex items-center gap-5",children:[t.jsx(e,{size:"sm",variant:"primary",endIcon:t.jsx(s,{className:"size-5"}),children:"Button Text"}),t.jsx(e,{size:"md",variant:"primary",endIcon:t.jsx(s,{className:"size-5"}),children:"Button Text"})]})}),t.jsx(i,{postData:o}),t.jsx(n,{title:"Secondary Button",children:t.jsxs("div",{className:"flex items-center gap-5",children:[t.jsx(e,{size:"sm",variant:"outline",children:"Button Text"}),t.jsx(e,{size:"md",variant:"outline",children:"Button Text"})]})}),t.jsx(i,{postData:m}),t.jsx(n,{title:"Outline Button with Left Icon",children:t.jsxs("div",{className:"flex items-center gap-5",children:[t.jsx(e,{size:"sm",variant:"outline",startIcon:t.jsx(s,{className:"size-5"}),children:"Button Text"}),t.jsx(e,{size:"md",variant:"outline",startIcon:t.jsx(s,{className:"size-5"}),children:"Button Text"})]})}),t.jsx(n,{title:"Outline Button with Right Icon",children:t.jsxs("div",{className:"flex items-center gap-5",children:[t.jsx(e,{size:"sm",variant:"outline",endIcon:t.jsx(s,{className:"size-5"}),children:"Button Text"}),t.jsx(e,{size:"md",variant:"outline",endIcon:t.jsx(s,{className:"size-5"}),children:"Button Text"})]})})]})]})}export{y as default};
--- assets/index-CxuK8ugS.js
+++
@@ -1,1 +0,0 @@
-import{bC as r,cX as p}from"./index-BvAjFZAV.js";var o=p();const e=r(o);export{e as P};
