5c923981b7e75638a34cf2e2399a30739503319f
park g s <deljjang@gmail.com>
2025-10-25 21:14:54 +0900

    home_2025-10-25

---
+++ assets/Images-CbMvT9SQ.js
@@ -0,0 +1,50 @@
+import{j as r,s as e}from"./index-CnztCwdv.js";import{C as i}from"./ComponentCard-CgqnqILF.js";import{P as d}from"./PageBreadCrumb-DtDcLpGT.js";import{b as a}from"./HtmlCode-79nAkhoJ.js";import{T as s}from"./TwTestButton-AIsy8o7v.js";import"./PdfFileViewer-DGv2DdlE.js";import"./HtmlText-41NvFjrL.js";import"./index-BSHbzSYL.js";/* empty css                   */import"./copy-CET_z54H.js";import"./TwBadge-wLevQGLz.js";import"./TwRadio-ea04VNpZ.js";import"./toPropertyKey-DCwh5dYN.js";import"./Button-CCvI0lsU.js";function g(){return r.jsx("div",{className:"relative",children:r.jsx("div",{className:"overflow-hidden",children:r.jsx("img",{src:"/images/grid-image/image-01.png",alt:"Cover",className:"w-full border border-gray-200 rounded-xl dark:border-gray-800"})})})}function m(){return r.jsxs("div",{className:"grid grid-cols-1 gap-5 sm:grid-cols-2 xl:grid-cols-3",children:[r.jsx("div",{children:r.jsx("img",{src:"/images/grid-image/image-04.png",alt:" grid",className:"border border-gray-200 rounded-xl dark:border-gray-800"})}),r.jsx("div",{children:r.jsx("img",{src:"/images/grid-image/image-05.png",alt:" grid",className:"border border-gray-200 rounded-xl dark:border-gray-800"})}),r.jsx("div",{children:r.jsx("img",{src:"/images/grid-image/image-06.png",alt:" grid",className:"border border-gray-200 rounded-xl dark:border-gray-800"})})]})}function o(){return r.jsxs("div",{className:"grid grid-cols-1 gap-5 sm:grid-cols-2",children:[r.jsx("div",{children:r.jsx("img",{src:"/images/grid-image/image-02.png",alt:" grid",className:"border border-gray-200 rounded-xl dark:border-gray-800"})}),r.jsx("div",{children:r.jsx("img",{src:"/images/grid-image/image-03.png",alt:" grid",className:"border border-gray-200 rounded-xl dark:border-gray-800"})})]})}function k(){return e({title:"Tailwind Images",description:"This is React.js Images page for TailAdmin - React.js Tailwind CSS Admin Dashboard Template"}),r.jsxs(r.Fragment,{children:[r.jsx(s,{}),r.jsx(d,{pageTitle:"Images"}),r.jsxs("div",{className:"space-y-5 sm:space-y-6",children:[r.jsxs(i,{title:"Responsive image",children:[r.jsx(g,{}),r.jsx(a,{postData:`<div className="relative">
+  <div className="overflow-hidden">
+    <img
+      src="/images/grid-image/image-01.png"
+      alt="Cover"
+      className="w-full border border-gray-200 rounded-xl dark:border-gray-800"
+    />
+  </div>
+</div>
+`})]}),r.jsxs(i,{title:"Image in 2 Grid",children:[r.jsx(o,{}),r.jsx(a,{postData:`<div className="grid grid-cols-1 gap-5 sm:grid-cols-2">
+  <div>
+    <img
+      src="/images/grid-image/image-02.png"
+      alt=" grid"
+      className="border border-gray-200 rounded-xl dark:border-gray-800"
+    />
+  </div>
+
+  <div>
+    <img
+      src="/images/grid-image/image-03.png"
+      alt=" grid"
+      className="border border-gray-200 rounded-xl dark:border-gray-800"
+    />
+  </div>
+</div>`})]}),r.jsxs(i,{title:"Image in 3 Grid",children:[r.jsx(m,{}),r.jsx(a,{postData:`<div className="grid grid-cols-1 gap-5 sm:grid-cols-2 xl:grid-cols-3">
+  <div>
+    <img
+      src="/images/grid-image/image-04.png"
+      alt=" grid"
+      className="border border-gray-200 rounded-xl dark:border-gray-800"
+    />
+  </div>
+
+  <div>
+    <img
+      src="/images/grid-image/image-05.png"
+      alt=" grid"
+      className="border border-gray-200 rounded-xl dark:border-gray-800"
+    />
+  </div>
+
+  <div>
+    <img
+      src="/images/grid-image/image-06.png"
+      alt=" grid"
+      className="border border-gray-200 rounded-xl dark:border-gray-800"
+    />
+  </div>
+</div>`})]})]})]})}export{k as default};
