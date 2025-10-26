a16f34452468129261e990eadfb6fd1a4e579355
park g s <deljjang@gmail.com>
2025-10-26 18:02:58 +0900

    home_2025-10-26

---
+++ assets/Buttons-iudHNcXu.js
@@ -0,0 +1,61 @@
+import{h as d,s as l,j as t}from"./index-D5kNWIwo.js";import{C as i}from"./ComponentCard-CY_CvxOt.js";import{P as x}from"./PageBreadCrumb-CXj7fiAf.js";import{H as n}from"./HtmlCode-DIWIu4Fy.js";import{B as e}from"./Button-6i7f83Co.js";import{S as s}from"./box-CtnnDa6S.js";import{T as u}from"./TwTestButton-BPL9QnXD.js";import"./PdfFileViewer-BuhMcFGq.js";import"./HtmlText-BDQrD17B.js";import"./index-CwOI0XVy.js";/* empty css                   */import"./copy-C-jsZ8Ld.js";import"./TwBadge-QXGko9GX.js";import"./TwRadio-Bhlcy5m2.js";import"./toPropertyKey-DCwh5dYN.js";function P(){const{t:a}=d(),o=`<code>React
+<ComponentCard title="Primary Button">
+  <div className="flex items-center gap-5">
+    <Button size="sm" variant="primary">
+      Button Text
+    </Button>
+    <Button size="md" variant="primary">
+      Button Text
+    </Button>
+  </div>
+</ComponentCard>
+</code>`,r=`<code>React
+<ComponentCard title="Primary Button with Left Icon">
+<div className="flex items-center gap-5">
+  <Button
+    size="sm"
+    variant="primary"
+    startIcon={<BoxIcon className="size-5" />}
+  >
+    Button Text
+  </Button>
+  <Button
+    size="md"
+    variant="primary"
+    startIcon={<BoxIcon className="size-5" />}
+  >
+    Button Text
+  </Button>
+</div>
+</ComponentCard>
+</code>`,c=`<code>React
+<ComponentCard title="Primary Button with Right Icon">
+  <div className="flex items-center gap-5">
+    <Button
+      size="sm"
+      variant="primary"
+      endIcon={<BoxIcon className="size-5" />}
+    >
+      Button Text
+    </Button>
+    <Button
+      size="md"
+      variant="primary"
+      endIcon={<BoxIcon className="size-5" />}
+    >
+      Button Text
+    </Button>
+  </div>
+</ComponentCard>
+</code>`,m=`<code>React
+<ComponentCard title="Secondary Button">
+  <div className="flex items-center gap-5">
+    <Button size="sm" variant="outline">
+      Button Text
+    </Button>
+    <Button size="md" variant="outline">
+      Button Text
+    </Button>
+  </div>
+</ComponentCard>
+</code>`;return l({title:"Tailwind Buttons",description:a("description")}),t.jsxs("div",{children:[t.jsx(u,{}),t.jsx(x,{pageTitle:"Buttons"}),t.jsxs("div",{className:"space-y-5 sm:space-y-6",children:[t.jsx(i,{title:"Primary Button",desc:" desc",children:t.jsxs("div",{className:"flex items-center gap-5",children:[t.jsx(e,{size:"sm",variant:"primary",children:"Button Text"}),t.jsx(e,{size:"md",variant:"primary",children:"Button Text"})]})}),t.jsx(n,{postData:o}),t.jsx(i,{title:"Primary Button with Left Icon",children:t.jsxs("div",{className:"flex items-center gap-5",children:[t.jsx(e,{size:"sm",variant:"primary",startIcon:t.jsx(s,{className:"size-5"}),children:"Button Text"}),t.jsx(e,{size:"md",variant:"primary",startIcon:t.jsx(s,{className:"size-5"}),children:"Button Text"})]})}),t.jsx(n,{postData:r}),t.jsx(i,{title:"Primary Button with Right Icon",children:t.jsxs("div",{className:"flex items-center gap-5",children:[t.jsx(e,{size:"sm",variant:"primary",endIcon:t.jsx(s,{className:"size-5"}),children:"Button Text"}),t.jsx(e,{size:"md",variant:"primary",endIcon:t.jsx(s,{className:"size-5"}),children:"Button Text"})]})}),t.jsx(n,{postData:c}),t.jsx(i,{title:"Secondary Button",children:t.jsxs("div",{className:"flex items-center gap-5",children:[t.jsx(e,{size:"sm",variant:"outline",children:"Button Text"}),t.jsx(e,{size:"md",variant:"outline",children:"Button Text"})]})}),t.jsx(n,{postData:m}),t.jsx(i,{title:"Outline Button with Left Icon",children:t.jsxs("div",{className:"flex items-center gap-5",children:[t.jsx(e,{size:"sm",variant:"outline",startIcon:t.jsx(s,{className:"size-5"}),children:"Button Text"}),t.jsx(e,{size:"md",variant:"outline",startIcon:t.jsx(s,{className:"size-5"}),children:"Button Text"})]})}),t.jsx(i,{title:"Outline Button with Right Icon",children:t.jsxs("div",{className:"flex items-center gap-5",children:[t.jsx(e,{size:"sm",variant:"outline",endIcon:t.jsx(s,{className:"size-5"}),children:"Button Text"}),t.jsx(e,{size:"md",variant:"outline",endIcon:t.jsx(s,{className:"size-5"}),children:"Button Text"})]})})]})]})}export{P as default};
--- assets/index-CnR2a0fI.js
+++
@@ -1,1 +0,0 @@
-import{be as r,bP as p}from"./index-DR721s7h.js";var o=p();const s=r(o);export{s as P};
