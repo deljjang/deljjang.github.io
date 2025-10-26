9afb141bd248ed07287504cd7836aab53b167a79
park g s <deljjang@gmail.com>
2025-10-26 18:14:32 +0900

    home_2025-10-26

---
+++ assets/Buttons-CGmdnxrx.js
@@ -0,0 +1,61 @@
+import{h as d,s as l,j as t}from"./index-D-tzgtCF.js";import{C as i}from"./ComponentCard-wyKML92S.js";import{P as x}from"./PageBreadCrumb-9GN1U-Io.js";import{H as n}from"./HtmlCode-BC4RUlp-.js";import{B as e}from"./Button-BsxAk4p8.js";import{S as s}from"./box-C88bYVmP.js";import{T as u}from"./TwTestButton-C56_rLjw.js";import"./PdfFileViewer-C8aHgY0m.js";import"./HtmlText-l7gvRljM.js";import"./index-DZ7f-X9a.js";/* empty css                   */import"./copy-Bf0RuaL0.js";import"./TwBadge-C17F5OXZ.js";import"./TwRadio-BucrabhZ.js";import"./toPropertyKey-DCwh5dYN.js";function P(){const{t:a}=d(),o=`<code>React
+<ComponentCard title="Primary Button">
+  <div className="flex items-center gap-5">
+    <Button size="sm" variant="primary">
+      Button Text
+    </Button>
+    <Button size="md" variant="primary">
+      Button Text
+    </Button>
+  </div>
+</ComponentCard>
+</code>`,r=`<code>React
+<ComponentCard title="Primary Button with Left Icon">
+<div className="flex items-center gap-5">
+  <Button
+    size="sm"
+    variant="primary"
+    startIcon={<BoxIcon className="size-5" />}
+  >
+    Button Text
+  </Button>
+  <Button
+    size="md"
+    variant="primary"
+    startIcon={<BoxIcon className="size-5" />}
+  >
+    Button Text
+  </Button>
+</div>
+</ComponentCard>
+</code>`,c=`<code>React
+<ComponentCard title="Primary Button with Right Icon">
+  <div className="flex items-center gap-5">
+    <Button
+      size="sm"
+      variant="primary"
+      endIcon={<BoxIcon className="size-5" />}
+    >
+      Button Text
+    </Button>
+    <Button
+      size="md"
+      variant="primary"
+      endIcon={<BoxIcon className="size-5" />}
+    >
+      Button Text
+    </Button>
+  </div>
+</ComponentCard>
+</code>`,m=`<code>React
+<ComponentCard title="Secondary Button">
+  <div className="flex items-center gap-5">
+    <Button size="sm" variant="outline">
+      Button Text
+    </Button>
+    <Button size="md" variant="outline">
+      Button Text
+    </Button>
+  </div>
+</ComponentCard>
+</code>`;return l({title:"Tailwind Buttons",description:a("description")}),t.jsxs("div",{children:[t.jsx(u,{}),t.jsx(x,{pageTitle:"Buttons"}),t.jsxs("div",{className:"space-y-5 sm:space-y-6",children:[t.jsx(i,{title:"Primary Button",desc:" desc",children:t.jsxs("div",{className:"flex items-center gap-5",children:[t.jsx(e,{size:"sm",variant:"primary",children:"Button Text"}),t.jsx(e,{size:"md",variant:"primary",children:"Button Text"})]})}),t.jsx(n,{postData:o}),t.jsx(i,{title:"Primary Button with Left Icon",children:t.jsxs("div",{className:"flex items-center gap-5",children:[t.jsx(e,{size:"sm",variant:"primary",startIcon:t.jsx(s,{className:"size-5"}),children:"Button Text"}),t.jsx(e,{size:"md",variant:"primary",startIcon:t.jsx(s,{className:"size-5"}),children:"Button Text"})]})}),t.jsx(n,{postData:r}),t.jsx(i,{title:"Primary Button with Right Icon",children:t.jsxs("div",{className:"flex items-center gap-5",children:[t.jsx(e,{size:"sm",variant:"primary",endIcon:t.jsx(s,{className:"size-5"}),children:"Button Text"}),t.jsx(e,{size:"md",variant:"primary",endIcon:t.jsx(s,{className:"size-5"}),children:"Button Text"})]})}),t.jsx(n,{postData:c}),t.jsx(i,{title:"Secondary Button",children:t.jsxs("div",{className:"flex items-center gap-5",children:[t.jsx(e,{size:"sm",variant:"outline",children:"Button Text"}),t.jsx(e,{size:"md",variant:"outline",children:"Button Text"})]})}),t.jsx(n,{postData:m}),t.jsx(i,{title:"Outline Button with Left Icon",children:t.jsxs("div",{className:"flex items-center gap-5",children:[t.jsx(e,{size:"sm",variant:"outline",startIcon:t.jsx(s,{className:"size-5"}),children:"Button Text"}),t.jsx(e,{size:"md",variant:"outline",startIcon:t.jsx(s,{className:"size-5"}),children:"Button Text"})]})}),t.jsx(i,{title:"Outline Button with Right Icon",children:t.jsxs("div",{className:"flex items-center gap-5",children:[t.jsx(e,{size:"sm",variant:"outline",endIcon:t.jsx(s,{className:"size-5"}),children:"Button Text"}),t.jsx(e,{size:"md",variant:"outline",endIcon:t.jsx(s,{className:"size-5"}),children:"Button Text"})]})})]})]})}export{P as default};
---
+++ assets/Images-DdddqRZ6.js
@@ -0,0 +1,50 @@
+import{j as r,s as e}from"./index-D-tzgtCF.js";import{C as i}from"./ComponentCard-wyKML92S.js";import{P as d}from"./PageBreadCrumb-9GN1U-Io.js";import{b as a}from"./HtmlCode-BC4RUlp-.js";import{T as s}from"./TwTestButton-C56_rLjw.js";import"./PdfFileViewer-C8aHgY0m.js";import"./HtmlText-l7gvRljM.js";import"./index-DZ7f-X9a.js";/* empty css                   */import"./copy-Bf0RuaL0.js";import"./TwBadge-C17F5OXZ.js";import"./TwRadio-BucrabhZ.js";import"./toPropertyKey-DCwh5dYN.js";import"./Button-BsxAk4p8.js";function g(){return r.jsx("div",{className:"relative",children:r.jsx("div",{className:"overflow-hidden",children:r.jsx("img",{src:"/images/grid-image/image-01.png",alt:"Cover",className:"w-full border border-gray-200 rounded-xl dark:border-gray-800"})})})}function m(){return r.jsxs("div",{className:"grid grid-cols-1 gap-5 sm:grid-cols-2 xl:grid-cols-3",children:[r.jsx("div",{children:r.jsx("img",{src:"/images/grid-image/image-04.png",alt:" grid",className:"border border-gray-200 rounded-xl dark:border-gray-800"})}),r.jsx("div",{children:r.jsx("img",{src:"/images/grid-image/image-05.png",alt:" grid",className:"border border-gray-200 rounded-xl dark:border-gray-800"})}),r.jsx("div",{children:r.jsx("img",{src:"/images/grid-image/image-06.png",alt:" grid",className:"border border-gray-200 rounded-xl dark:border-gray-800"})})]})}function o(){return r.jsxs("div",{className:"grid grid-cols-1 gap-5 sm:grid-cols-2",children:[r.jsx("div",{children:r.jsx("img",{src:"/images/grid-image/image-02.png",alt:" grid",className:"border border-gray-200 rounded-xl dark:border-gray-800"})}),r.jsx("div",{children:r.jsx("img",{src:"/images/grid-image/image-03.png",alt:" grid",className:"border border-gray-200 rounded-xl dark:border-gray-800"})})]})}function k(){return e({title:"Tailwind Images",description:"This is React.js Images page for TailAdmin - React.js Tailwind CSS Admin Dashboard Template"}),r.jsxs(r.Fragment,{children:[r.jsx(s,{}),r.jsx(d,{pageTitle:"Images"}),r.jsxs("div",{className:"space-y-5 sm:space-y-6",children:[r.jsxs(i,{title:"Responsive image",children:[r.jsx(g,{}),r.jsx(a,{postData:`<div className="relative">
+  <div className="overflow-hidden">
+    <img
+      src="/images/grid-image/image-01.png"
+      alt="Cover"
+      className="w-full border border-gray-200 rounded-xl dark:border-gray-800"
+    />
+  </div>
+</div>
+`})]}),r.jsxs(i,{title:"Image in 2 Grid",children:[r.jsx(o,{}),r.jsx(a,{postData:`<div className="grid grid-cols-1 gap-5 sm:grid-cols-2">
+  <div>
+    <img
+      src="/images/grid-image/image-02.png"
+      alt=" grid"
+      className="border border-gray-200 rounded-xl dark:border-gray-800"
+    />
+  </div>
+
+  <div>
+    <img
+      src="/images/grid-image/image-03.png"
+      alt=" grid"
+      className="border border-gray-200 rounded-xl dark:border-gray-800"
+    />
+  </div>
+</div>`})]}),r.jsxs(i,{title:"Image in 3 Grid",children:[r.jsx(m,{}),r.jsx(a,{postData:`<div className="grid grid-cols-1 gap-5 sm:grid-cols-2 xl:grid-cols-3">
+  <div>
+    <img
+      src="/images/grid-image/image-04.png"
+      alt=" grid"
+      className="border border-gray-200 rounded-xl dark:border-gray-800"
+    />
+  </div>
+
+  <div>
+    <img
+      src="/images/grid-image/image-05.png"
+      alt=" grid"
+      className="border border-gray-200 rounded-xl dark:border-gray-800"
+    />
+  </div>
+
+  <div>
+    <img
+      src="/images/grid-image/image-06.png"
+      alt=" grid"
+      className="border border-gray-200 rounded-xl dark:border-gray-800"
+    />
+  </div>
+</div>`})]})]})]})}export{k as default};
