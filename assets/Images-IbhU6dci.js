0870576d734d040943259ed564f47d96802b3f82
park g s <deljjang@gmail.com>
2025-12-09 22:02:17 +0900

    home_2025-12-09

---
+++ assets/Images-IbhU6dci.js
@@ -0,0 +1,50 @@
+import{j as r}from"./index-Ck2d_L_e.js";import{s as d,ae as a,bn as e}from"./App--a2e3x7R.js";import{P as i}from"./PageBreadCrumb-eZPHNAhz.js";import{T as s}from"./TwTestButton-CbXUFzuV.js";import"./router-BlMcuVHC.js";import"./vendor-CCYiWZgt.js";function g(){return r.jsx("div",{className:"relative",children:r.jsx("div",{className:"overflow-hidden",children:r.jsx("img",{src:"/images/grid-image/image-01.png",alt:"Cover",className:"w-full border border-gray-200 rounded-xl dark:border-gray-800"})})})}function m(){return r.jsxs("div",{className:"grid grid-cols-1 gap-5 sm:grid-cols-2 xl:grid-cols-3",children:[r.jsx("div",{children:r.jsx("img",{src:"/images/grid-image/image-04.png",alt:" grid",className:"border border-gray-200 rounded-xl dark:border-gray-800"})}),r.jsx("div",{children:r.jsx("img",{src:"/images/grid-image/image-05.png",alt:" grid",className:"border border-gray-200 rounded-xl dark:border-gray-800"})}),r.jsx("div",{children:r.jsx("img",{src:"/images/grid-image/image-06.png",alt:" grid",className:"border border-gray-200 rounded-xl dark:border-gray-800"})})]})}function o(){return r.jsxs("div",{className:"grid grid-cols-1 gap-5 sm:grid-cols-2",children:[r.jsx("div",{children:r.jsx("img",{src:"/images/grid-image/image-02.png",alt:" grid",className:"border border-gray-200 rounded-xl dark:border-gray-800"})}),r.jsx("div",{children:r.jsx("img",{src:"/images/grid-image/image-03.png",alt:" grid",className:"border border-gray-200 rounded-xl dark:border-gray-800"})})]})}function p(){return d({title:"Tailwind Images",description:"This is React.js Images page for TailAdmin - React.js Tailwind CSS Admin Dashboard Template"}),r.jsxs(r.Fragment,{children:[r.jsx(s,{}),r.jsx(i,{pageTitle:"Images"}),r.jsxs("div",{className:"space-y-5 sm:space-y-6",children:[r.jsxs(a,{title:"Responsive image",children:[r.jsx(g,{}),r.jsx(e,{postData:`<div className="relative">
+  <div className="overflow-hidden">
+    <img
+      src="/images/grid-image/image-01.png"
+      alt="Cover"
+      className="w-full border border-gray-200 rounded-xl dark:border-gray-800"
+    />
+  </div>
+</div>
+`})]}),r.jsxs(a,{title:"Image in 2 Grid",children:[r.jsx(o,{}),r.jsx(e,{postData:`<div className="grid grid-cols-1 gap-5 sm:grid-cols-2">
+  <div>
+    <img
+      src="/images/grid-image/image-02.png"
+      alt=" grid"
+      className="border border-gray-200 rounded-xl dark:border-gray-800"
+    />
+  </div>
+
+  <div>
+    <img
+      src="/images/grid-image/image-03.png"
+      alt=" grid"
+      className="border border-gray-200 rounded-xl dark:border-gray-800"
+    />
+  </div>
+</div>`})]}),r.jsxs(a,{title:"Image in 3 Grid",children:[r.jsx(m,{}),r.jsx(e,{postData:`<div className="grid grid-cols-1 gap-5 sm:grid-cols-2 xl:grid-cols-3">
+  <div>
+    <img
+      src="/images/grid-image/image-04.png"
+      alt=" grid"
+      className="border border-gray-200 rounded-xl dark:border-gray-800"
+    />
+  </div>
+
+  <div>
+    <img
+      src="/images/grid-image/image-05.png"
+      alt=" grid"
+      className="border border-gray-200 rounded-xl dark:border-gray-800"
+    />
+  </div>
+
+  <div>
+    <img
+      src="/images/grid-image/image-06.png"
+      alt=" grid"
+      className="border border-gray-200 rounded-xl dark:border-gray-800"
+    />
+  </div>
+</div>`})]})]})]})}export{p as default};
---
+++ assets/TwMain-qYALNUgs.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-Ck2d_L_e.js";import{r as s,e as a,f as o}from"./router-BlMcuVHC.js";import{r as p,L as m}from"./App--a2e3x7R.js";import"./vendor-CCYiWZgt.js";const l=()=>r.jsx(s.Suspense,{fallback:r.jsx(m,{}),children:r.jsx(a,{children:p.map((e,t)=>r.jsx(o,{path:e.path,element:e.element},t))})});export{l as default};
